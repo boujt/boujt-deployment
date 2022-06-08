104a34a2585b1d85d861e97f4852adf4aad89309
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-06-08 09:51:27 +0200

    backend: feat: added quiz + vanliga frågor

---
+++ backend/src/api/vanliga-fragor/controllers/vanliga-fragor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  vanliga-fragor controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::vanliga-fragor.vanliga-fragor');
