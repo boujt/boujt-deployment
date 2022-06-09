6d061da41d42efb8f923cb29f12f83723c1097c8
Jakob Karlstrand <jakobkarlstrand@gmail.com>
2022-06-09 13:35:31 +0200

    Created Request collections

---
+++ backend/src/api/chat-request/routes/chat-request.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * chat-request router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::chat-request.chat-request');
