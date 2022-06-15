8ecd1194c5003b94674e402856f9a3a59645248c
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-06-15 11:20:02 +0200

    chore: logo is now clickable

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -10,67 +10,69 @@ import OmOss from "../../public/tecken-video/om_oss.gif";
 import { VideoTooltip } from "./VideoTooltip";
 import { ROUTES } from "../../utils/constants";
 type NavbarItemProps = {
-  text: string;
-  route: string;
+	text: string;
+	route: string;
 };
 const NavbarItem: NextPage<NavbarItemProps> = (props: NavbarItemProps) => {
-  return (
-    <Link href={props.route}>
-      <Text
-        noOfLines={1}
-        marginTop={"auto"}
-        marginBottom={"auto"}
-        variant="link"
-        fontSize={{ base: "14px", md: "18px", lg: "22px" }}
-        padding={3}
-      >
-        {props.text}
-      </Text>
-    </Link>
-  );
+	return (
+		<Link href={props.route}>
+			<Text
+				noOfLines={1}
+				marginTop={"auto"}
+				marginBottom={"auto"}
+				variant="link"
+				fontSize={{ base: "14px", md: "18px", lg: "22px" }}
+				padding={3}
+			>
+				{props.text}
+			</Text>
+		</Link>
+	);
 };
 
 const Navbar: NextPage = () => {
-  return (
-    <Flex
-      maxW={"100%"}
-      w="100vw"
-      background={"blackish"}
-      justifyContent="space-evenly"
-      wrap="wrap"
-      p={8}
-    >
-      <Image
-        width={"171px"}
-        height={"47px"}
-        src="/images/icon.png"
-        alt="boujt logo"
-      />
-      <Flex>
-        <VideoTooltip src={Kroppen.src}>
-          <NavbarItem text="Känslor och rättigheter" route="/kor" />
-        </VideoTooltip>
+	return (
+		<Flex
+			maxW={"100%"}
+			w="100vw"
+			background={"blackish"}
+			justifyContent="space-evenly"
+			wrap="wrap"
+			p={8}
+		>
+			<Link href="/">
+				<Image
+					width={"171px"}
+					height={"47px"}
+					src="/images/icon.png"
+					alt="boujt logo"
+				/>
+			</Link>
+			<Flex>
+				<VideoTooltip src={Kroppen.src}>
+					<NavbarItem text="Känslor och rättigheter" route="/kor" />
+				</VideoTooltip>
 
-        <VideoTooltip src={Frageladan.src}>
-          <NavbarItem text="Frågelådan" route={ROUTES.FRAGELADA} />
-        </VideoTooltip>
-        <VideoTooltip src={Stod.src}>
-          <NavbarItem text="Få stöd" route={ROUTES.FA_STOD} />
-        </VideoTooltip>
-        <VideoTooltip src={OmOss.src}>
-          <NavbarItem text="Om oss" route={ROUTES.OM_OSS} />
-        </VideoTooltip>
+				<VideoTooltip src={Frageladan.src}>
+					<NavbarItem text="Frågelådan" route={ROUTES.FRAGELADA} />
+				</VideoTooltip>
+				<VideoTooltip src={Stod.src}>
+					<NavbarItem text="Få stöd" route={ROUTES.FA_STOD} />
+				</VideoTooltip>
+				<VideoTooltip src={OmOss.src}>
+					<NavbarItem text="Om oss" route={ROUTES.OM_OSS} />
+				</VideoTooltip>
 
-        <VideoTooltip src={Kontakt.src}>
-          <NavbarItem text="Kontakta oss" route={ROUTES.KONTAKTA_OSS} />
-        </VideoTooltip>
+				<VideoTooltip src={Kontakt.src}>
+					<NavbarItem text="Kontakta oss" route={ROUTES.KONTAKTA_OSS} />
+				</VideoTooltip>
 
-        <VideoTooltip src={GomBesok.src}>
-          <NavbarItem text="Göm ditt besök" route={ROUTES.GOM_BESOK} />
-        </VideoTooltip>
-      </Flex>
-    </Flex>
-  );
+				<VideoTooltip src={GomBesok.src}>
+					<NavbarItem text="Göm ditt besök" route={ROUTES.GOM_BESOK} />
+				</VideoTooltip>
+			</Flex>
+		</Flex>
+	);
 };
 
 export default Navbar;
