446e10ee3f9ed3e6843f5948003a25c881dee09d
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-06-22 09:59:43 +0200

    chore: backend mods

--- backend/src/api/videochat/routes/videochat.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * videochat router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::videochat.videochat');
