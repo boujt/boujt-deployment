87f893f5d974d73e361d5ef5a5d81694766eedba
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-06-22 19:02:03 +0200

    Revert "Merge branch 'deploy' into unstable"
    This reverts commit 1944085ef062bc79dfd82a49492e5bd4a519634b.

--- backend/src/api/request-chat/controllers/request-chat.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  request-chat controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::request-chat.request-chat');
--- backend/src/api/request-chat/services/request-chat.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * request-chat service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::request-chat.request-chat');
