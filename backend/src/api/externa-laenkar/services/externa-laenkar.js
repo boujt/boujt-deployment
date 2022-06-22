034d143d1de741424741ad484828cb5b4d4111c4
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-06-22 20:55:29 +0200

    added external links

---
+++ backend/src/api/externa-laenkar/services/externa-laenkar.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * externa-laenkar service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::externa-laenkar.externa-laenkar');
