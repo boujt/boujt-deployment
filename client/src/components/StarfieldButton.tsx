a8ac10ce61216d4b9808dc5e2194b7cea05180b3
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-06-22 14:44:33 +0200

    feat: made kanslor-och-rattigheter more interactable

--- client/src/components/StarfieldButton.tsx
+++ client/src/components/StarfieldButton.tsx
@@ -1,61 +1,73 @@
-import { Box, Flex, Heading, Icon, Text } from "@chakra-ui/react";
-import { CSSProperties } from "react";
+import { Box, Flex, Heading, Icon, useToast } from "@chakra-ui/react";
+import { CSSProperties, useEffect, useState } from "react";
 import { IconType } from "react-icons";
+import { under_construction_config } from "../theme";
 import Starfield from "./Starfield";
 
 const background: CSSProperties = {
-	position: 'absolute',
+	position: "absolute",
 	left: 0,
 	top: 0,
 	width: "100%",
 	height: "100%",
-	zIndex: -100
-}
+	zIndex: -100,
+};
 
 type Props = {
-    text: string,
-    icon: IconType,
-    iconColor: string,
-    width: number,
-    onClicked?: () => void
-}
+	text: string;
+	icon: IconType;
+	iconColor: string;
+	width: number;
+	onClicked?: () => void;
+};
 
 // Clickable button with starfield background
-const StarfieldButton: React.FC<Props> = ({width, text, icon, iconColor, onClicked}) => {
-    return (
-        <Flex 
-            width={width} 
-            minHeight={width*0.3}
-            position={'relative'} 
-            justifyContent={'center'}
-            borderRadius={'20px'}
-            onClick={onClicked}
-            cursor={'pointer'}
-        >
-            <Flex
-                width={'85%'}
-                justifyContent={'space-between'}
-                alignItems={'center'} 
-            >
-                <Heading color={'white'}>
-                    {text}
-                </Heading>
-                <Icon 
-                    w={12}
-                    h={12}
-                    color={iconColor}
-                    as={icon}
-                />
-            </Flex>
-            <Box sx={background} borderRadius={'20px'}>
-                <Starfield 
-                    starCount={20}
-                    boxProps={{borderRadius: '20px'}} 
-                    sx={{borderRadius: '20px'}}
-                />
-            </Box>
-        </Flex>
-    )
-}
+const StarfieldButton: React.FC<Props> = ({
+	width,
+	text,
+	icon,
+	iconColor,
+	onClicked,
+}) => {
+	const [mouseOver, setMouseOver] = useState(false);
+
+	const toast = useToast();
+
+	const onLocalClick = () => {
+		// @ts-ignore
+		toast(under_construction_config);
+	};
+
+	return (
+		<Flex
+			width={width}
+			minHeight={width * 0.3}
+			position={"relative"}
+			justifyContent={"center"}
+			borderRadius={"20px"}
+			boxShadow={mouseOver ? "0px 0px 20px #FDE30F" : "none"}
+			onClick={onClicked ? onClicked : onLocalClick}
+			cursor={"pointer"}
+			onMouseOver={() => setMouseOver(true)}
+			onMouseLeave={() => setMouseOver(false)}
+		>
+			<Flex
+				width={"85%"}
+				justifyContent={"space-between"}
+				alignItems={"center"}
+			>
+				<Heading color={"white"}>{text}</Heading>
+				<Icon w={12} h={12} color={iconColor} as={icon} />
+			</Flex>
+			<Box sx={background} borderRadius={"20px"}>
+				<Starfield
+					starCount={20}
+					boxProps={{ borderRadius: "20px" }}
+					sx={{ borderRadius: "20px" }}
+				/>
+			</Box>
+		</Flex>
+	);
+};
 
 export default StarfieldButton;
