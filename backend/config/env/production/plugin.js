5531de9eaa399448e45d8303f381d05c2665f3cb
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-06-29 13:25:10 +0200

    chore: removed directory to DOS config

--- backend/config/env/production/plugin.js
+++ backend/config/env/production/plugin.js
@@ -7,8 +7,7 @@ module.exports = ({env}) => ({
           key: env('DO_SPACE_ACCESS_KEY'),
           secret: env('DO_SPACE_SECRET_KEY'),
           endpoint: env('DO_SPACE_ENDPOINT'),
-          space: env('DO_SPACE_BUCKET'),
-          directory: "media", // optional
+          space: env('DO_SPACE_BUCKET')
         }
       },
     }, 
