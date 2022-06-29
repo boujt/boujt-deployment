8e74936eb4c99237d152b6642c22aa7ddecea722
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-06-29 11:39:13 +0200

    dospace

--- backend/config/env/production/plugin.js
+++ backend/config/env/production/plugin.js
@@ -4,10 +4,10 @@ module.exports = ({env}) => ({
       config: {
         provider: "strapi-provider-upload-do", 
         providerOptions: {
-          key: process.env.DO_SPACE_ACCESS_KEY,
-          secret: process.env.DO_SPACE_SECRET_KEY,
-          endpoint: process.env.DO_SPACE_ENDPOINT,
-          space: process.env.DO_SPACE_BUCKET,
+          key: env('DO_SPACE_ACCESS_KEY'),
+          secret: env('DO_SPACE_SECRET_KEY'),
+          endpoint: env('DO_SPACE_ENDPOINT'),
+          space: env('DO_SPACE_BUCKET'),
           directory: "media", // optional
         }
       },
