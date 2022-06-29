15c3af95209e5eb11cc75606f7c52b58ab831b5d
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-06-29 13:06:35 +0200

    middleware

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -11,9 +11,14 @@ module.exports = [
             "'self'",
             "data:",
             "blob:",
-            "*.digitaloceanspaces.com"
+            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
           ],
-          "media-src": ["'self'", "data:", "blob:"],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
+          ],
           upgradeInsecureRequests: null,
         },
       },
