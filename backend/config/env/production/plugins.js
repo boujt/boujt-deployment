70ecd14074a4e9f0f2007232f1519916f3b842dd
Jakob Karlstrand <jakobkarlstrand@gmail.com>
2022-06-30 17:44:17 +0200

    Added sendgrid provider for email

--- backend/config/env/production/plugins.js
+++ backend/config/env/production/plugins.js
@@ -1,15 +1,28 @@
-module.exports = ({env}) => ({
-    // ...
-    upload: {
-      config: {
-        provider: "strapi-provider-upload-do", 
-        providerOptions: {
-          key: env('DO_SPACE_ACCESS_KEY'),
-          secret: env('DO_SPACE_SECRET_KEY'),
-          endpoint: env('DO_SPACE_ENDPOINT'),
-          space: env('DO_SPACE_BUCKET')
-        }
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: env("DO_SPACE_ACCESS_KEY"),
+        secret: env("DO_SPACE_SECRET_KEY"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        space: env("DO_SPACE_BUCKET"),
       },
-    }, 
-    // ...
-  })
+    },
+  },
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "boujt.videos@gmail.com",
+        defaultReplyTo: "boujt.videos@gmail.com",
+        testAddress: "boujt.videos@gmail.com",
+      },
+    },
+  },
+  // ...
+});
